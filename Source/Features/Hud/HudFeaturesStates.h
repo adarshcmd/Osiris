455b85bfb593d37a73cd824f3ea6e11276a5e7c1
Adarsh <enderonmoon@gmail.com>
2026-06-26 16:41:05 +0530

    Store watermark HUD state

--- Source/Features/Hud/HudFeaturesStates.h
+++ Source/Features/Hud/HudFeaturesStates.h
@@ -4,10 +4,12 @@
 #include "BombPlantAlert/BombPlantAlertState.h"
 #include "DefusingAlert/DefusingAlertState.h"
 #include "PostRoundTimer/PostRoundTimerState.h"
+#include "Watermark/WatermarkState.h"
 
 struct HudFeaturesStates {
     BombTimerState bombTimerState;
     DefusingAlertState defusingAlertState;
     PostRoundTimerState postRoundTimerState;
     BombPlantAlertState bombPlantAlertState;
+    WatermarkState watermarkState;
 };
